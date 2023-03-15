9c9b4226ddb048cc704de68f5efa8ee9609b0fe4
robiul0278 <robiul0278@gmail.com>
2023-03-16 00:22:29 +0600

    show all display question

--- index.js
+++ index.js
@@ -46,10 +46,9 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch(`./data/quiz.json`);
-  const data = await res.json;
+  const data = await res.json();
   quizData = data;
   displayQuiz(data);
-  console.log(data)
 };
 
 // Displaying quiz on quiz page
@@ -58,14 +57,13 @@ const displayQuiz = (data) => {
     quizContainer.innerHTML = "";
     return;
   }
-
   data.forEach((quiz, i) => {
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
       ${i + 1}
     </div>
-    <p class="text-gray-800 text-sm">${quiz.question}</p>
+    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
   </div>
   <div class="grid grid-cols-2 gap-4 mt-5">
     ${displayQuizOptions(quiz.options, i)}
